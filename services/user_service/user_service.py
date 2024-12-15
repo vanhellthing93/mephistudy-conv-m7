70f679513efd8de409868a82a266bc3ae44fbfa3
VanHellthing <vanhellthing@mail.ru>
2024-12-15 19:09:56 +0300

    changed logic

--- services/user_service/user_service.py
+++ services/user_service/user_service.py
@@ -1,21 +1,22 @@
 import grpc
 import user_service_pb2
 import user_service_pb2_grpc
+import order_service_pb2_grpc
 from concurrent import futures
 
 class UserService(user_service_pb2_grpc.UserServiceServicer):
     def CreateUser(self, request, context):
-        # Логика создания пользователя
-        pass
+        user_id = "user_" + request.email
+        return user_service_pb2.UserResponse(user_id=user_id, name=request.name, email=request.email)
 
     def GetUser(self, request, context):
-        # Логика получения пользователя
-        pass
-
+        # Для примера возвращаем фиктивные данные
+        return user_service_pb2.UserResponse(user_id=request.user_id, name="John Doe", email="john.doe@example.com")
+    
     def SendMessage(self, request, context):
         with grpc.insecure_channel('order_service:5002') as channel:
             stub = order_service_pb2_grpc.OrderServiceStub(channel)
-            response = stub.ReceiveMessage(order_service_pb2.MessageRequest(message=request.message))
+            response = stub.ReceiveMessage(order_service_pb2_grpc.MessageRequest(message=request.message))
             print(f"UserService sent message: {request.message}")
             return user_service_pb2.MessageResponse(status="Message sent")
 
